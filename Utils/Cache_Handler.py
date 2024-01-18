2ce8842361afe3b493c0fa75d2f5c9184b5f22d1
Lukas Berghegger <lukasberghegger@gmail.com>
2024-01-18 13:04:49 +0100

    added more documentation

--- Utils/Cache_Handler.py
+++ Utils/Cache_Handler.py
@@ -6,7 +6,14 @@ from platformdirs import user_cache_dir
 
 class CacheHandler:
     def __init__(self, name: str, author: str) -> None:
-        print("loading cash for " + author + ", " + name)
+        """
+        Creates a cache object that can read and write JSON files to the standard cache directory \n
+        Access using the indexing operator [] as a dict \n
+        will automatically store all contained information on application exit
+        :param name: name of the Application
+        :param author: Application Author to determine cache path
+        """
+        print("loading cache for " + author + ", " + name)
 
         self.data = {}
         self.path = None
@@ -14,9 +21,17 @@ class CacheHandler:
         self.load(name, author)
 
     def __del__(self):
+        """
+        saves cache on object delete
+        """
         self.save()
 
-    def __getitem__(self, key: str):
+    def __getitem__(self, key: str) -> object:
+        """
+        Index function for cache object todo
+        :param key: key of requested dataset
+        :return: returns object of indeterminate type from dict
+        """
         try:
             return self.data[key]
         except KeyError:
@@ -25,7 +40,13 @@ class CacheHandler:
     def __setitem__(self, key: str, value):
         self.data[key] = value
 
-    def load(self, name: str, author: str):
+    def load(self, name: str, author: str) -> None:
+        """
+        loads cache object from file manually, will replace any previous contents!
+        
+        :param name: name of the Application
+        :param author: Application Author to determine cache path
+        """
         self.path = user_cache_dir(name, author)
         os.makedirs(self.path, exist_ok=True)
 
@@ -39,6 +60,9 @@ class CacheHandler:
             print("creating new cache file")
             self.data = {}
 
-    def save(self):
+    def save(self) -> None:
+        """
+        saves cache to file
+        """
         print("storing cache at " + self.path + "\n")
         json.dump(self.data, open(self.path + "/settings.json", mode="w", encoding="utf-8"))
